1906b2e03f9a4831844ba0537d04bec7679b5093
Dinesh Kumar Choudhary <mapmydestination@gmail.com>
2021-02-14 04:36:28 +0530

    update

--- index.php
+++ index.php
@@ -106,7 +106,7 @@
 
   <!-- Main Footer -->
   <footer class="main-footer">
-    <strong>Copyright &copy; 2020 <a href="https://www.sourcecodester.com/">sourcecodester.com</a>.</strong>
+    <strong>Copyright &copy; 2020 <a>Simple Online Survey System</a>.</strong>
     All rights reserved.
     <div class="float-right d-none d-sm-inline-block">
       <b>Online Survey System</b>
